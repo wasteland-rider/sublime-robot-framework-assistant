a295380a30f0a37bae5360f098a74258226b778c
tanakiat <tanakiats@gmail.com>
2019-02-14 01:29:00 +0700

    handle byte check for md5hash

--- dataparser/parser_utils/file_formatter.py
+++ dataparser/parser_utils/file_formatter.py
@@ -3,7 +3,7 @@ from os import path
 
 
 def rf_table_name(f_path):
-    md5sum = md5(f_path.encode('utf-8')).hexdigest()
+    md5sum = md5(f_path.encode('utf-8') if isinstance(f_path, str) else f_path).hexdigest()
     return '{realname}-{md5}.json'.format(
         realname=path.basename(f_path)[-100:],
         md5=md5sum
